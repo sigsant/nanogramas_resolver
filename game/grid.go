2cc0cba9d3cd5d709ad95f08e98b46eec612bbac
Heros <eleusis@duck.com>
2022-08-20 15:41:39 +0200

    Create Nanogram grid

--- game/grid.go
+++ game/grid.go
@@ -1,17 +1,39 @@
 package game
 
 import (
-	"fmt"
-
 	_ "github.com/sigsant/nonogramas_resolver/util/logger"
 )
 
-type Grid struct {
-	Columns int
-	Rows    int
+type nonogram struct {
+	grid  [][]int
+	hints [][]int
 }
 
-func (g *Grid) String() string {
-	// logger.DebugLogger.Printf("Grid creado con dimensiones %dx%d", g.Columns, g.Rows)
-	return fmt.Sprintf("Grid creado con dimensiones %dx%d", g.Columns, g.Rows)
+//Por ejemplo, si se rellenara con un 5x5 el resultado
+//inicial es el siguiente array multidmensional:
+// &{[[0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0] [0 0 0 0 0]]}
+
+// ó para visualizarlo mejor:
+
+// [0 0 0 0 0]
+// [0 0 0 0 0]
+// [0 0 0 0 0]
+// [0 0 0 0 0]
+// [0 0 0 0 0]
+
+func createArrMultidimensional(rows, columns int) [][]int {
+	multiarr := make([][]int, rows)
+	for i := range multiarr {
+		multiarr[i] = make([]int, columns)
+	}
+	return multiarr
+}
+
+// CreateGrid crea un duplicado del casillero que se rellenará posteriormente para incluir las guías de dibujo
+func CreateGrid(rows, columns int) *nonogram {
+	return &nonogram{
+		grid: createArrMultidimensional(rows, columns),
+		//Las guias siempre seran un 5x5
+		hints: createArrMultidimensional(5, 5),
+	}
 }
